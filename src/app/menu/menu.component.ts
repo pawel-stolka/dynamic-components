1fba8ab2ca06e4e6948e0c1ea525f08e25101376
FP\pstolka <pstolka@future-processing.com>
2022-07-10 22:43:36 +0200

    step 1

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,20 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-menu',
   template: `
-    <p>
-      menu works!
-    </p>
-  `,
-  styles: [
-  ]
+    <button mat-icon-button aria-label="Button to open menu" [matMenuTriggerFor]="menu">
+      <mat-icon>menu</mat-icon>
+    </button>
+    <mat-menu #menu="matMenu">
+      <button mat-menu-item *ngIf="!isAuthenticated" (click)="login()">
+        <mat-icon>login</mat-icon> <span>Login</span>
+      </button>
+      <button mat-menu-item *ngIf="isAuthenticated" (click)="logout()">
+        <mat-icon>logout</mat-icon> <span>Logout</span>
+      </button>
+    </mat-menu>
+  `
 })
-export class MenuComponent implements OnInit {
 
-  constructor() { }
+// https://developer.okta.com/blog/2021/12/08/angular-dynamic-components
+export class MenuComponent implements OnInit, OnDestroy {
+  public isAuthenticated = false;
+  private _destroySub$ = new Subject<void>();
 
-  ngOnInit(): void {
+  constructor(
+    private _oktaAuth: OktaAuth,
+    private _authStateService: OktaAuthStateService,
+    private _router: Router
+  ) { }
+
+  public ngOnInit(): void {
+   this._authStateService.authState$.pipe(
+     filter((s: AuthState) => !!s),
+     map((s: AuthState) => s.isAuthenticated ?? false),
+     distinctUntilChanged(),
+     takeUntil(this._destroySub$)
+   ).subscribe(
+     (authenticated: boolean) => this.isAuthenticated = authenticated
+   );
   }
 
+  public ngOnDestroy(): void {
+    this._destroySub.next();
+  }
+
+  public async login(): Promise<void> {
+    await this._oktaAuth.signInWithRedirect().then(
+      _ => this._router.navigate(['/protected'])
+    );
+  }
+
+  public async logout(): Promise<void> {
+    await this._oktaAuth.signOut();
+  }
 }
